46fa3ef795ea142ab422381fda95dd92065b790e
Prateek Choudavarpu <prateek.10201@gmail.com>
2024-12-29 20:03:02 -0500

    chore:config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  compiler: {
+    // Remove all console logs
+    removeConsole:
+      process.env.NEXT_PUBLIC_NODE_ENV === "production" ? true : false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  staticPageGenerationTimeout: 180,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
